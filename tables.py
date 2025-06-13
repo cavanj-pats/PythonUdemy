028ca5dd3270119756bd4bc64102f18a27d19338
Jim Cavanaugh <cavanj@protonmail.com>
2025-06-13 00:04:22 -0400

    tablespy regex implemented added Series and block type

--- tables.py
+++ tables.py
@@ -5,24 +5,29 @@ import camelot
 import pypdfium
 import csv
 import pandas as pd
+import re     #regex for pattern match
 
 
 #header  scale ('10,)
 header = camelot.read_pdf("C:/Users/james/Downloads/10785_Solid-Block-Mounted-SRB-Catalog_LR.pdf",
                       flavor='stream'  ,  #'lattice',
-                      pages='94-95' ,
+                      pages='94-138, 141-180' ,
                       table_areas = ['5,800,550,720'] ,
                       flag_size = True,    
                       strip_text = '\n' ,
-                      #line_scale = 50 ,
-                      #shift_text = ['t'],
-                      #copy_text =['v'],
                       layout_kwargs = {'detect_vertical' : True} )
 
+""" footer = camelot.read_pdf("C:/Users/james/Downloads/10785_Solid-Block-Mounted-SRB-Catalog_LR.pdf",
+                      flavor='stream'  , 
+                      pages='94-138' ,
+                      table_areas = ['5,50,550,5'] ,
+                      flag_size = True,    
+                      strip_text = '\n' ,
+                      layout_kwargs = {'detect_vertical' : True} ) """
 
 a = camelot.read_pdf("C:/Users/james/Downloads/10785_Solid-Block-Mounted-SRB-Catalog_LR.pdf",
                       flavor='lattice',
-                      pages='94-95' ,
+                      pages='94-138, 141-180' ,
                       table_regions = ['25,500,560,180'] ,
                       #columns = ['35, 49, 62, 71, 79, 88, 96, 105, 104, 113, 122, 131, 139, 148, 156, 165, 173, 182, 190 '],
                       split_text = False,  #was true can change back if needed
@@ -34,37 +39,67 @@ a = camelot.read_pdf("C:/Users/james/Downloads/10785_Solid-Block-Mounted-SRB-Cat
 
 #camelot.plot(a[0], kind = 'grid', filename='grid')
 #camelot.plot(a[0], kind = 'text', filename='text')
-numTables = a.n   #number of tables access for 0 to n-1
+#numTables = a.n   #number of tables access for 0 to n-1
+firstTable = True
 
-headerData = header[0].df
-#a[0]df[0][2] finds the data in column 0 and row 2
+for t in range(len(a)):
+    headerData = header[t].df
+        #a[0]df[0][2] finds the data in column 0 and row 2
 
-#this works but also picks up the period in 'mmin.'
-dataSeries = (a[0].df)[3]   #column index 3/ might also be teh name of the column
-for i in range(dataSeries.size):
-    position = dataSeries[i].find('.' )   #should find the first one.
-    if position != -1:
-        pos = position + 2
-        dataSeries[i]= dataSeries[i][:pos] + ' / ' + dataSeries[i][pos:]
+        #this works but also picks up the period in 'mmin.'
+    cols = a[t].df.shape[1]   # i think this is columns  [1] is columns [0] is rows
+    rows = a[t].df.shape[0]
+    row = 0
+    for c in range(cols):
+        dataSeries = (a[t].df)[c]   #column index 3/ might also be teh name of the column
+        head = a[t].df[c][0]   #should be the column header from catalog
+        for i in range(dataSeries.size):
+            match = re.match('[0-9]{1,3}.[0-9]{2,3}.[0-9]{2}',dataSeries[i])
+            if match:
+                position = dataSeries[i].find('.' )  #should find the first one.
+                if position != -1:
+                    pos = position + 2
+                    dataSeries[i]= dataSeries[i][:pos] + ' / ' + dataSeries[i][pos:]
+            elif head == 'J' and i > 0:
+                #this is a bolt size
+                pos = 2   # bolt in milimeters is always two characters
+                dataSeries[i] = dataSeries[i][:pos] + 'mm  ' + dataSeries[i][pos:] + 'in.'
+            elif head == 'Wt.' and i > 1:
+                position = dataSeries[i].find('.' )  #should find the first one.
+                if position != -1:
+                    pos = position + 2
+                    dataSeries[i]= dataSeries[i][:pos] + ' / ' + dataSeries[i][pos:]    
+            else:
+                continue
 
-(a[0].df)[3] = dataSeries
-cols = a[0].df.shape[0]   # i think this is columns
+        (a[t].df)[c] = dataSeries
 
-a[0].df.insert(cols, 'Series', headerData.values[0][0])
-a[0].df.insert(cols+1,'Block_Type', headerData.values[1][0])
-
-# Print first table
-print(a[0].df)
+    if t == 0 and firstTable:
+        a[t].df.insert(cols,'18','')
+        a[t].df.insert(cols+1,'19','')
+        a[t].df.insert(cols+2,'20','')
+        a[t].df.insert(cols+3,'21','')
+        a[t].df.insert(cols+4,'22','')
+        a[t].df.insert(cols+5, 'Series', headerData.values[0][t])
+        a[t].df.insert(cols+6,'Block_Type', headerData.values[1][t])
+        a[t].df.to_csv('tmp_file.txt' )
+        firstTable = False
+    else:
+        a[t].df['Series']= headerData.values[0][0]
+        a[t].df['Block_Type'] = headerData.values[1][0]
+        a[t].df.to_csv('tmp_file.txt', mode='a', header=False )
 
 
 #with open('tmp_file.txt', 'w') as f:
-for i in range(len(a)):
+""" for i in range(len(a)):
     if i== 0:
         a[i].df.to_csv('tmp_file.txt' )
     else:
-        a[i].df.to_csv('tmp_file.txt', mode='a', header=False )
+        a[i].df.to_csv('tmp_file.txt', mode='a', header=False ) """
        
-       
+ # Print first table
+#print(a[0].df)
+      
        
         #print(f"Index: {i}, Value: {my_array[i]}")
 #        csv.writer(f, delimiter=',').writerows(a[i])
