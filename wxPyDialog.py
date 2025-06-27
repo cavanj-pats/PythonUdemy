ebfba638eb143eecac036e1eb755db82fe096492
Jim Cavanaugh <cavanj@protonmail.com>
2025-06-26 21:13:16 -0400

    wx widgets example

---
+++ wxPyDialog.py
@@ -0,0 +1,18 @@
+#wxPyDialog.py
+
+
+
+# First things, first. Import the wxPython package.
+import wx
+
+# Next, create an application object.
+app = wx.App()
+
+# Then a frame.
+frm = wx.Frame(None, title="Hello World")
+
+# Show it.
+frm.Show()
+
+# Start the event loop.
+app.MainLoop()
