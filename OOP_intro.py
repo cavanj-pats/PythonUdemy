f449de3893a87357334ab4f06d0445b891d03af3
Jim Cavanaugh <cavanj@protonmail.com>
2026-02-07 11:37:25 -0500

    Intro OOP

---
+++ OOP_intro.py
@@ -0,0 +1,17 @@
+
+#
+#   introduction to objects
+"""
+    Class hs
+        Properties  / Data Members
+        Methods   /  Member Functions
+        
+        this achieves
+            encapsulation
+            abstraction
+            data hiding
+            
+            ...
+            inheritance
+            polymorphism
+"""
