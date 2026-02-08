16861064ea4e83450c613f7bf598181fa103730b
Jim Cavanaugh <cavanj@protonmail.com>
2026-02-08 14:21:16 -0500

    Object Challenge Even Planner - use Abdul Code/approach

--- challenge_EventPlanner.py
+++ challenge_EventPlanner.py
@@ -28,15 +28,30 @@ class EventPlanner:
 
 
 if __name__ == "__main__":
-    curYear = 2026
+    
+    curYear = int(input('Enter Year: '))
     ep = EventPlanner(curYear)
+    """
     tempDate = dt.datetime(curYear, 4, 30, 16, 30, 0)
     ep.add_event(tempDate, 'Send OSR')
     tempDate = dt.datetime(curYear, 6, 5, 16, 30, 0)
     ep.add_event(tempDate, 'First Part Delivery')
     tempDate = dt.datetime(curYear, 7, 29, 16, 30, 0)
     ep.add_event(tempDate, 'Part 2 and 3 Delivery')
+    """
+    
+        #Abdul used input
 
+    for i in range(3):    #add three events
+        date = [int(x) for x in input('Enter Date dd/mm/yyy ').split('/')]
+        time = [int(x) for x in input('Enter Time hr:min ').split(':')]
+        when = dt.datetime(date[2], date[1], date[0], time[0], time[1])   #needed to swap time[0] and time[1] from Abdul screen
+        details = input('Detials of Event: ')
+        ep.add_event(when, details)
+    
     ep.list_events()
+
+
+    
 
 
