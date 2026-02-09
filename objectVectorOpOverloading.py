ed432b58f257de84d536923d6cb37649247d970f
Jim Cavanaugh <cavanj@protonmail.com>
2026-02-08 19:53:09 -0500

    Object Concrete Interface Abstract notes added

--- objectVectorOpOverloading.py
+++ objectVectorOpOverloading.py
@@ -1,5 +1,9 @@
 #objectVectorOpOverloading.py
 
+#parent type classes can be
+# Concrete classes - have some or all of the intended functionality that child classes access
+# Interface - function interfaces are defined by functionality is deferred to child.
+#abstract - mixture of Concrete and Interface 
 
 
 class Vector:
@@ -14,10 +18,14 @@ class Vector:
         y = self.y + other.y
         return Vector(x, y)
     
+    #this is how to create a function to be used when "print" is invoked
+    def __str__(self):
+        return f"({str(self.x)},{str(self.y)})"
 
 
 
 v1 = Vector(2,2)
 v2 = Vector(1,5)
 v3 = v1+v2
-print(v3.x, v3.y)
+#print(v3.x, v3.y)
+print(v3)
