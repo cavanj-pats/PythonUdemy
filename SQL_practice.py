eb3f8c0b86481fe0055301243f5da57c61d3984b
Jim Cavanaugh <cavanj@protonmail.com>
2026-02-16 18:39:14 -0500

    Python SQL practice minor update

--- SQL_practice.py
+++ SQL_practice.py
@@ -1,24 +1,35 @@
 #SQL_practice.py
 import sqlite3
 
+
+def createTable():
+    conn = sqlite3.connect('univ2.db')
+
+    cursor = conn.cursor()
+
+    cursor.execute('create table dept(deptno integer primary key,' 
+            'dname text)')
+
+    cursor.execute('create table students (roll integer primary key,' 
+                'name text,'
+                'city text,'
+                    'deptno integer,'
+                    'foreign key(deptno)'
+                    'references dept(deptno))')
+
+    conn.commit()
+
+    cursor.close()
+
+    conn.close()
+
+
+
+
 conn = sqlite3.connect('univ2.db')
 
 cursor = conn.cursor()
-
-cursor.execute('create table dept(deptno integer primary key,' 
-        'dname text)')
-
-cursor.execute('create table students (roll integer primary key,' 
-               'name text,'
-               'city text,'
-                'deptno integer,'
-                 'foreign key(deptno)'
-                  'references dept(deptno))')
-
+cursor.execute('insert into dept calues(10, "CSE")')
 conn.commit()
-
 cursor.close()
-
 conn.close()
-
-
