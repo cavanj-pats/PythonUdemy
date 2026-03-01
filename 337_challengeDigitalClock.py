d698bfe4523609870bd2fdac9183f9b89bdb7eae
Jim Cavanaugh <cavanj@protonmail.com>
2026-03-01 13:18:38 -0500

    Python TK Challenge Digital Clock minor tweaks and comments

--- 337_challengeDigitalClock.py
+++ 337_challengeDigitalClock.py
@@ -9,7 +9,7 @@ import time
 #from tkmacosx import *
 
 def myclock():
-    str_time = time.strftime('%H:%M:%S')
+    str_time = time.strftime('%H:%M:%S') #%I would be 12 hour clock
     lbl1['text'] = str_time
     lbl1.after(100, myclock)
 
@@ -19,7 +19,7 @@ win = Tk()
 win.title('First Application!')
 win.geometry('600x400')   #add +100+10 places the windowget()
 
-#var = StringVar()
+#ff5555 would have red and some green and some blue.   RGB
 lbl1 = Label(win, bg='red', font=('courier', 40 ), fg='yellow' )
 lbl1.pack(fill=BOTH, expand=True)  #not 'both' and not 1 vs BOTH and True
 
