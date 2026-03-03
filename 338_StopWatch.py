ae713e4dca5cf49d8157551974318c90c80d04fb
Jim Cavanaugh <cavanj@protonmail.com>
2026-03-02 20:48:43 -0500

    Python TK Challenge Stop Watch - used Abdul code framework. Needed to assign elapse time = 0  in main

--- 338_StopWatch.py
+++ 338_StopWatch.py
@@ -10,26 +10,35 @@ import datetime
 #from tkmacosx import *
 
 def update_time():
-    global running, elapsed_time
-    if running:
-        elapsed_time += time.time()
-        lbl1['text'] = ms.strftime('%M:%S:%f'[:-3])
-        lbl1.after = (10, btnStart_Click())
+    global flag, elapse_time
+    if flag == True:
+        elapse_time += 10
+        milli = elapse_time % 1000
+        sec = elapse_time // 1000
+        min = sec // 60
+        sec = sec % 60
+
+        lbl1['text'] = f'{min:02d}:{sec:02d}:{milli:03d}'
+        lbl1.after(10, update_time)
        
   
 
 def btnStart_Click():
-    global running, elapsed_time, start_time
-    running = True
+    global flag, elapse_time
+    flag = True
     
-
     update_time()
-        
-        lbl1['text'] = ms.strftime('%M:%S:%f'[:-3])
-        lbl1.after = (10, btnStart_Click())
-    #stop when stop when stop is pushed
-    #reset when reset is pushed
+      
 
+def stop():
+    global flag
+    flag = False
+
+def reset():
+    global elapse_time, flag
+    elapse_time=0
+    flag = False
+    lbl1['text']='00:00:000'
     
     
 
@@ -41,7 +50,8 @@ win.title('Stop watch challenge Application!')
 win.geometry('400x200')   #add +100+10 places the windowget()
 
 
-
+global elapse_time
+elapse_time = 0
 
 #ff5555 would have red and some green and some blue.   RGB
 lbl1 = Label(win, bg='red', font=('courier', 30 ), fg='yellow', text='00:00:000' )
@@ -52,10 +62,10 @@ lbl1.grid(column=0,columnspan=3,row=0)  #grid column, columnspan row
 btnStart = Button(win, text='Start', command=btnStart_Click)
 btnStart.grid(column=0,columnspan=1,row=1)
 
-btnStop = Button(win, text='Stop')
+btnStop = Button(win, text='Stop', command=stop)
 btnStop.grid(column=1,columnspan=1,row=1)
 
-btnReset = Button(win, text='Reset')
+btnReset = Button(win, text='Reset', command=reset)
 btnReset.grid(column = 2,columnspan=1,row=1)
 
 
