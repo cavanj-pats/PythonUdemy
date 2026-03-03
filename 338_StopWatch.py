faf0a76d47325f7ccb8fc2eb960fe9452f833037
Jim Cavanaugh <cavanj@protonmail.com>
2026-03-02 20:10:34 -0500

    Python TK Challenge Stop Watch dev

--- 338_StopWatch.py
+++ 338_StopWatch.py
@@ -3,53 +3,32 @@
 
 from tkinter import *  # type: ignore
 import time
+import datetime
 
 #from tkinter.font import *
 
 #from tkmacosx import *
 
-class Stopwatch:
-    def __init__(self):
-        self._start_time=None
-        self._elapsed_time = 0
-
-    def start(self):
-        if self._start_time is None:
-            self._start_time = time.time()
-
-    def stop(self):
-        if self._start_time is not None:
-            tp =  time.time() - self._start_time
-            self._elapsed_time += tp
-            self._start_time is None
-
-
-    def reset(self):
-        self._start_time = None
-        self._elapsed_time = 0
-
-
-    def get_elapsed_time(self):
-        return self._elapsed_time
-
-    def display_time(self):
-        return time.ctime()
-    
-
+def update_time():
+    global running, elapsed_time
+    if running:
+        elapsed_time += time.time()
+        lbl1['text'] = ms.strftime('%M:%S:%f'[:-3])
+        lbl1.after = (10, btnStart_Click())
+       
   
 
 def btnStart_Click():
-  # reset clock
-  #start clock
-  #run clock
-    if not running:
-        running = True
-        sw.reset
+    global running, elapsed_time, start_time
+    running = True
+    
 
-    lbl1['text'] = sw.get_elapsed_time()
-    lbl1.after = (10, btnStart_Click())
-  #stop when stop when stop is pushed
-  #reset when reset is pushed
+    update_time()
+        
+        lbl1['text'] = ms.strftime('%M:%S:%f'[:-3])
+        lbl1.after = (10, btnStart_Click())
+    #stop when stop when stop is pushed
+    #reset when reset is pushed
 
     
     
@@ -62,8 +41,7 @@ win.title('Stop watch challenge Application!')
 win.geometry('400x200')   #add +100+10 places the windowget()
 
 
-sw = Stopwatch()
-running = False
+
 
 #ff5555 would have red and some green and some blue.   RGB
 lbl1 = Label(win, bg='red', font=('courier', 30 ), fg='yellow', text='00:00:000' )
