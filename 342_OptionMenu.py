86267348d2ced45abc6ff625b0eabf814255739e
Jim Cavanaugh <cavanj@protonmail.com>
2026-03-04 11:50:52 -0500

    Python TK Challenge OptionMenu complete and working but maybe not the same as Abdul

--- 342_OptionMenu.py
+++ 342_OptionMenu.py
@@ -2,20 +2,38 @@
 
 from tkinter import *  # type: ignore
 
+def optSelection(value):
+    #change the list box data based on selection of optionmenu
+    listData.set(optDict[value])
+
+
 
 win = Tk()
 win.title('Option Menu Challenge')
 win.geometry('400x300')   #add +100+10 places the windowget()
 
-optText = ["Languages", "Databases", "Clouds"]
+#the options presented to the user
+optText = ["Languages", "Databases", "Clouds"]   #this variable is used below to set default listbox contents
 
-drill = dict({ [ ["Languages"] , ['C++', 'Python', 'C Lang'] ],
-         [ ["Databases"] , ['Oracle', 'SQLite', 'MongoDB']],
-          ["Clouds"] , ['Azure', 'iCloud', 'DropBox'] })
+#the data associated with the selected option
+data = [ ['C++', 'Python', 'C Lang'] ,   ['Oracle', 'SQLite', 'MongoDB'], ['Azure', 'iCloud', 'DropBox'] ]
 
-optMnu1 = OptionMenu(win, variable=optText,  *drill ) 
+optDict = dict(zip(optText, data))   #[*optDict] #uses list comprehensions to return a list of keys unpack operator
+
+optVar = StringVar()
+optVar.set([*optDict][0])
+
+#from tkdocs  OptionMenu(parent, variable, choice1, choice2,....)  #used *choice to show the collection
+optMnu1 = OptionMenu(win, optVar,  *optDict, command=optSelection ) 
 optMnu1.pack()
 
+
+#build the list box
+listData = StringVar()
+#the default selection will be [*opDict][0] so teh default listbox contents should be based on that
+listData.set(optDict[optText[0]])    #default lisbox contents based on default selection of optionmenu
+lst1 = Listbox(win, listvariable=listData)
+lst1.pack()
 
 
 
