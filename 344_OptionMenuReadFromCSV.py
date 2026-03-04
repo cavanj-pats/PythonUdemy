932e435aff1b3923e1961f62e2f9c6adab49763e
Jim Cavanaugh <cavanj@protonmail.com>
2026-03-04 16:20:04 -0500

    Python TK Challenge Read Data added Abdul code for reference

--- 344_OptionMenuReadFromCSV.py
+++ 344_OptionMenuReadFromCSV.py
@@ -56,34 +56,38 @@ win.mainloop()
 """
         ABDUL CODE FROM GITHUB
 
-                            from tkinter import *
+                         from tkinter import *
+            import csv
 
 
-                    def change(e):
-                        lstvar = Variable(value=dict1[var1.get()])
-                        lst1.delete(0, END)
-                        lst1['listvariable'] = lstvar
+            def show_data(e):
+                i = headings.index(var1.get())
+                col = [row[i] for row in data]
+                lst1.delete(0, END)
+                lst1['listvariable'] = Variable(value=col)
 
 
-                    win = Tk()
-                    win.geometry('400x300')
+            win = Tk()
+            win.geometry('400x300')
 
-                    dict1 = {'Languages': ['C', 'C++', 'Java'],
-                            'Databases': ['Oracle', 'SQL Server', 'mysql', 'Mango DB'],
-                            'Clouds': ['AWS', 'SalesForce', 'MS Asure']}
+            file_csv = open('Employees.csv', 'r')
+            reader = csv.reader(file_csv)
 
-                    var1 = StringVar(value='Languages')
+            headings = next(reader)
+            data = []
+            for t in reader:
+                data.append(t)
 
-                    opt1 = OptionMenu(win,  var1, *dict1.keys(), command=change)
-                    opt1['fg'] = 'blue'
+            var1 = StringVar(value=headings[0])
+            opt1 = OptionMenu(win, var1, *headings, command=show_data)
+            opt1.pack()
 
-                    opt1.pack()
+            eids = [row[0] for row in data]
 
-                    lstvar1 = Variable(value=dict1['Languages'])
+            lstvar1 = Variable(value=eids)
+            lst1 = Listbox(win, listvariable=lstvar1)
+            lst1.pack()
 
-                    lst1 = Listbox(win, listvariable=lstvar1)
-                    lst1.pack()
-
-                    win.mainloop()
+            win.mainloop()   
 
 """
