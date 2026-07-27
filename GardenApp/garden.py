0e6a6fc9e153afed6d7cb5e10e51cf231742beb7
Jim Cavanaugh <cavanj@protonmail.com>
2026-07-26 21:04:53 -0400

    Garden App

--- GardenApp/garden.py
+++ GardenApp/garden.py
@@ -39,11 +39,14 @@ class gardenApp(tk.Tk):
         self.mainFrame = tk.Frame(self, bg="light green")
         self.mainFrame.pack()
 
+        self.sprayFrame = sprayCalcs(self, self.mainFrame)
+        self.sprayFrame.pack()
+        self.sprayFrame.tkraise()
 
 
 
 class fuelMix(tk.Frame):
-    def __init__(self, parent, controller):
+    def __init__(self, parent):
         super().__init__(parent)
 
         #  1) You have fuel amount and desired fuel/oil ratio
@@ -85,6 +88,59 @@ class sprayCalcs(tk.Frame):
         super().__init__(parent)
 
         #other code here
+        self.varMixVolume = tk.StringVar()
+        self.lblAmount = tk.Label(self, text="Mix: ")
+        self.lblAmount.grid(row=0, column=0)
+        self.valAmount = tk.Entry(self, textvariable=self.varMixVolume)
+        self.valAmount.grid(row=0, column=1)
+        self.lstMixUnits = tk.Listbox(self)
+        self.lstMixUnits.insert(0,'ounces')
+        self.lstMixUnits.insert(1, 'TBS')
+        self.lstMixUnits.insert(2,'Gallon')
+        self.lstMixUnits.grid(row=0, column=2)
+
+        self.varNameProduct = tk.StringVar()
+        self.lblProduct = tk.Label(self, text='Product')
+        self.lblProduct.grid(row=1, column=0)
+        self.valProduct = tk.Entry(self, textvariable=self.varNameProduct)
+        self.valProduct.grid(row=1, column=1)
+
+        self.varMixRate = tk.StringVar()
+        self.lblMixRate = tk.Label(self, text='Label Mix Rate: ')
+        self.lblMixRate.grid(row=2, column=0)
+
+        self.MixRate = tk.Entry(self, textvariable=self.varMixRate)
+        self.MixRate.grid(row=2, column=1)
+        
+        self.lblProductUnits = tk.Label(self, text='ounces / gallon')
+        self.lblProductUnits.grid(row=2, column=2)
+
+        self.btnCalculate=tk.Button(self, text='Calculate', command=self.calculate)
+        self.btnCalculate.grid(row=3, column=2)
+
+        self.varResult = tk.StringVar()
+        self.result = tk.Entry(self, textvariable=self.varResult)
+        self.result.grid(row=4, column=1)
+
+    def calculate(self):
+        totalVolume = int(self.varMixVolume.get())
+        rate = int(self.varMixRate.get())
+
+        #rate will be ounces per gallon to keep it simple for now
+
+        index = self.lstMixUnits.get(self.lstMixUnits.curselection())
+
+        if index == 'ounces' :
+            #totalVolume * rate / 128 ounces per gallong
+            self.varResult.set(f"{totalVolume * rate / 128} ounces of product to make {totalVolume} ounces")
+        elif index == 1:
+            pass
+        else:
+            pass
+
+
+
+
 
 
 
