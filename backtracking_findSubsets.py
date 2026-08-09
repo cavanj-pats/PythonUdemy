6389f1a0abe48ae9feeaa1b4c1eae2f2c161aacb
Jim Cavanaugh <cavanj@protonmail.com>
2026-08-08 20:09:25 -0400

    backtracking practice find all subsets

--- backtracking_findSubsets.py
+++ backtracking_findSubsets.py
@@ -4,17 +4,28 @@
 
 nums = [1,2,3]
 ans, sol = [], []
+i = 0
+def backtrack(i):
 
-def backtrack():
-
-
-    if sol == nums:
-        #ans.append(sol[:]) #this is a deep copy not a reference copy
-        return
-    for i in nums:
+    
+        #there are two options.   exclude or include
+    if i == len(nums):
         ans.append(sol[:])
-        sol=i
-        backtrack
+        #ans.append([]) # the algorithm will not return a null list but it is a valid subst
+        return
+    else:
+        #include result
+        #ans.append(sol[:])
+        sol.append(nums[i]) #include 1 in solution
+        backtrack(i+1)
+        sol.pop() #exclude i in solution
+        backtrack(i+1)
+    
 
 
+        
+
+
+
+backtrack(0)
 print(ans)
